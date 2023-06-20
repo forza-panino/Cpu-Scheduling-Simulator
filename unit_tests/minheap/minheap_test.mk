1a8972991588fdb94361e806512ec20d6ffad99c
forza-panino <matteo.casani01@gmail.com>
2023-06-20 20:32:39 +0200

    added unit test to heap with specialise item

--- unit_tests/minheap/minheap_test.mk
+++ unit_tests/minheap/minheap_test.mk
@@ -3,19 +3,23 @@ CCOPTS=--std=gnu99 -Wall -D_HEAP_DEBUG_
 AR=ar
 
 OBJS=test_with_generics.o
-EXT_OBJS=minheap.o
+EXT_OBJS=minheap.o\
+		fake_process.o\
+		linked_list.o
 
 EXT_DIR=../../
 
-HEADERS=$(EXT_DIR)minheap.h
+HEADERS=$(EXT_DIR)minheap.h\
+		$(EXT_DIR)fake_process.h\
+		$(EXT_DIR)linked_list.h
 
-BINS=test_with_generics
+BINS=test_with_generics test_with_specialised
 
 .phony: clean all
 
 
 all:	$(BINS)
-	chmod +x $(BINS).out
+	chmod +x $(addsuffix .out, $(BINS))
 
 $(filter-out $(EXT_OBJS), %.o):	%.c $(HEADERS)
 	$(CC) $(CCOPTS) -c -o $@  $<
@@ -26,5 +30,8 @@ $(filter-out $(OBJS), %.o):	$(EXT_DIR)%.c $(HEADERS)
 test_with_generics:	test_with_generics.c $(EXT_OBJS)
 	$(CC) $(CCOPTS) -o $@.out $^
 
+test_with_specialised:	test_with_specialised.c $(EXT_OBJS)
+	$(CC) $(CCOPTS) -o $@.out $^
+
 clean:
-	rm -rf *.o *~ $(OBJS) $(BINS)
+	rm -rf *.o *~ $(OBJS) $(EXT_OBJS) $(BINS)
---
+++ unit_tests/minheap/test_with_specialised.c
@@ -0,0 +1,56 @@
+#include "../../minheap.h"
+#include "../../fake_process.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct {
+    HeapItem heapItem;
+    FakeProcess process;
+} FakeProcessHeapItem;
+
+int main(int argc, char** argv) {
+
+	FakeProcessHeapItem heapItems[4] = {
+    {{.key = 0}, {.pid = 0}},
+    {{.key = 1}, {.pid = 1}},
+    {{.key = 2}, {.pid = 2}},
+    {{.key = 3}, {.pid = 3}}
+    };
+
+    Heap heap;
+	Heap_init(&heap);
+
+    Heap_insert(&heap, (HeapItem*) &heapItems[0]);
+    Heap_insert(&heap, (HeapItem*) &heapItems[1]);
+    Heap_insert(&heap, (HeapItem*) &heapItems[2]);
+    Heap_insert(&heap, (HeapItem*) &heapItems[3]);
+
+    assert(heap.items[0]->key==0);
+    assert(heap.items[1]->key==1);
+    assert(heap.items[2]->key==2);
+    assert(heap.items[3]->key==3);
+
+    HeapItem* min = Heap_extractMin(&heap);
+    assert(min->key==0);
+    FakeProcess* process = &(((FakeProcessHeapItem*)min)->process);
+    assert(process->pid==0);
+
+    min = Heap_extractMin(&heap);
+    assert(min->key==1);
+    process = &(((FakeProcessHeapItem*)min)->process);
+    assert(process->pid==1);
+
+    min = Heap_extractMin(&heap);
+    assert(min->key==2);
+    process = &(((FakeProcessHeapItem*)min)->process);
+    assert(process->pid==2);
+
+    min = Heap_extractMin(&heap);
+    assert(min->key==3);
+    process = &(((FakeProcessHeapItem*)min)->process);
+    assert(process->pid==3);
+
+    min = Heap_extractMin(&heap);
+    assert(min==NULL);
+}
